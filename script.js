4409b134e6e5050c5ae1a31389a5238c59f9db5a
Navneet_thakur <127828104+navneet451@users.noreply.github.com>
2024-11-03 16:34:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function() {
+            // Check if there are saved credentials in local storage
+            const savedUsername = localStorage.getItem('username');
+            const savedPassword = localStorage.getItem('password');
+            const existingButton = document.getElementById('existing');
+	if (savedUsername && savedPassword) {
+                existingButton.style.display = 'block';
+            }
+	document.getElementById('loginForm').addEventListener('submit', function(event) {
+                event.preventDefault(); // Prevent page reload
+
+                const username = document.getElementById('username').value;
+                const password = document.getElementById('password').value;
+                const rememberMe = document.getElementById('checkbox').checked;
+		 if (rememberMe) {
+                    localStorage.setItem('username', username);
+                    localStorage.setItem('password', password);
+                } else {
+                    localStorage.removeItem('username');
+                    localStorage.removeItem('password');
+                }
+		 alert(`Logged in as ${username}`);
+		if (rememberMe) {
+                    existingButton.style.display = 'block';
+                } else {
+                    existingButton.style.display = 'none';
+                }
+	});
+		
+});
+	function loginAsExistingUser() {
+            const username = localStorage.getItem('username');
+            if (username) {
+                alert(`Logged in as ${username}`);
+            }
+        }
